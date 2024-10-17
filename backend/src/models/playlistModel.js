d5d0050bde9731c340bf60b5f11d86c9d53465e5
bonpaul <legiahuy51tambinh@gmail.com>
2024-10-17 22:59:08 +0700

    Added new models, route and controllers
    - Added song and playlist models
    - Added mongoDB and Cloudinary configuration
    - Trying song controllers and multers

---
+++ backend/src/models/playlistModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema({
+    name: { type: String, require: true },
+    desc: { type: String, require: true },
+    bgColor: { type: String, require: true },
+    image: { type: String, require: true },
+})
+
+const playlistModel = mongoose.models.playlist || mongoose.model("playlist", playlistSchema);
+
+export default playlistModel;
